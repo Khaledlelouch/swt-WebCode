577208b2a5707da450fbffe4452201d895d7d719
Muhammad Majdi Abdul Ghni <62240319+majdi-91@users.noreply.github.com>
2021-01-21 13:55:40 +0100

    Update create-patient.component.ts

--- src/app/create-patient/create-patient.component.ts
+++ src/app/create-patient/create-patient.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-create-patient',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-patient.component.css']
 })
 export class CreatePatientComponent implements OnInit {
+  createPatientForm: FormGroup;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.createPatientForm = new FormGroup({
+      vorname: new FormControl(null),
+      nachname: new FormControl(null),
+      geburtstag: new FormControl(null),
+      einzugsDatum: new FormControl(null),
+      strasse: new FormControl(null),
+      hausnr: new FormControl(null),
+      postleitzahl: new FormControl(null),
+      ort: new FormControl(null),
+      mobilnummer: new FormControl(null),
+      allergien: new FormControl(null),
+      ernaehrungstyp: new FormControl(null),
+      zimmerNummer: new FormControl(null),
+      stationNummer: new FormControl(null),
+      erkrankung: new FormControl(null)
+    });
   }
+  // tslint:disable-next-line:typedef
+  onSubmit(){
+
+  }
+  // tslint:disable-next-line:typedef
+  dashBoard(){
+    this.router.navigate(['/patient']);
+  }
+
 
 }
